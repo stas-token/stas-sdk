9b3a7a6a2662b13c609af5c05eaaab0e12b451f5
cainnusti <120074889+cainnusti@users.noreply.github.com>
2023-08-04 01:32:30 +1000

    Return un-serialized transactions
    As ARC broadcast endpoints are poised to become the new standard, we're updating our library to make the conversion to the extended format seamless, negating the necessity of handling additional input data for this process.
    
    For optimal usage of ARC, the transaction must be converted to the extended format, which requires the scriptPubKey and satoshi value for each input. By ensuring the non-serialized transaction is returned, these components will already be included within the transaction returned by the functions. This eliminates the need for any subsequent intervention to reintroduce this data before transitioning to the extended format.
    
    For more information on the ARC transaction processor please see this documentation
    https://bitcoin-sv.github.io/arc/#/
    
    To continue using the library as per normal it will require that transactions are serialised once returned from the functions.
    
    In addition a bug fix has been added for a utility function regarding BN.js library

--- tests/nonBroadcastFullCycleTest.js
+++ tests/nonBroadcastFullCycleTest.js
@@ -43,40 +43,40 @@ const fullCycleTestNonBroadcast = async () => {
 
   // Create a contract transaction of the token. Will create a token contract for a supply of 10 tokens.
   const contractHex = await stasContract.signed(issuerPrivateKey, utxoTemplateForContract, utxoTemplateForFees, paymentPrivateKey, tokenSchemaTemplate, 10);
-  console.log('Contract Transaction Hex: ', contractHex);
+  console.log('Contract Transaction Hex: ', contractHex.toString());
   const contractUtxo = utility.getUtxoFromTx(contractHex, 0);
 
 
   // Issue the token from the contract UTXO. This will be a STAS-20 token type.
   const issueHex = await stasIssuance.signed(issuerPrivateKey, issueData, contractUtxo, utxoTemplateForFees, paymentPrivateKey, true, tokenSchemaTemplate.symbol, 'STAS-20');
-  console.log('Issue Transaction Hex: ', issueHex);
+  console.log('Issue Transaction Hex: ', issueHex.toString());
   const tokenUtxoFromIssuance = utility.getUtxoFromTx(issueHex, 0);
 
 
   // Transfer the token to the same owner address.
   const transferHex1 = await stasTransfer.signed(tokenOwnerPrivateKey, tokenUtxoFromIssuance, tokenOwnerAddress, utxoTemplateForFees, paymentPrivateKey );
-  console.log('Transfer1 Transaction Hex: ', transferHex1);
+  console.log('Transfer1 Transaction Hex: ', transferHex1.toString());
   const tokenUtxoFromTransfer1 = utility.getUtxoFromTx(transferHex1, 0);
 
 
   // Split the token into two UTXOs of 5 satoshis each
   const splitDestinationsForSplitTx1 = [{address: tokenOwnerAddress, satoshis: 5}, {address: tokenOwnerAddress, satoshis: 5}];
   const splitHex1 = await stasSplit.signed(tokenOwnerPrivateKey, tokenUtxoFromTransfer1, splitDestinationsForSplitTx1, utxoTemplateForFees, paymentPrivateKey );
-  console.log('Split1 Transaction Hex: ', splitHex1);
+  console.log('Split1 Transaction Hex: ', splitHex1.toString());
 
 
   // Merge the two 5 satoshi tokens back into a single UTXO. For this we require the whole previous transaction hex and vout value for both STAS UTXO inputs.
   const stasData1 = {txHex: splitHex1, vout: 0};
   const stasData2 = {txHex: splitHex1, vout: 1};
   const mergeHex = await stasMerge.signed(tokenOwnerPrivateKey, stasData1, tokenOwnerPrivateKey, stasData2, tokenOwnerAddress, paymentPrivateKey, utxoTemplateForFees);
-  console.log('Merge Transaction Hex: ', mergeHex);
+  console.log('Merge Transaction Hex: ', mergeHex.toString());
   const tokenUtxoFromMergeTx = utility.getUtxoFromTx(mergeHex, 0);
 
 
   // Split the token into two UTXOs one of 9 satoshis and one of 1 satoshi
   const splitDestinationsForSplitTx2 = [{address: tokenOwnerAddress, satoshis: 9}, {address: tokenOwnerAddress, satoshis: 1}];
   const splitHex2 = await stasSplit.signed(tokenOwnerPrivateKey, tokenUtxoFromMergeTx, splitDestinationsForSplitTx2, utxoTemplateForFees, paymentPrivateKey );
-  console.log('Split2 Transaction Hex: ', splitHex2);
+  console.log('Split2 Transaction Hex: ', splitHex2.toString());
 
 
   // MergeSplit will take two UTXO inputs and then allow it to be split into more than one output. We will merge the UTXOs of 9 satoshi and 1 satoshis and create two new outputs with 2 satoshis and 8 satoshis
@@ -84,19 +84,19 @@ const fullCycleTestNonBroadcast = async () => {
   const stasData4 = {txHex: splitHex2, vout: 1};
   const splitDestinationsForMergeSplitTx = [{address: tokenOwnerAddress, satoshis: 2}, {address: tokenOwnerAddress, satoshis: 8}];
   const mergeSplitHex = await stasMergeSplit.signed(tokenOwnerPrivateKey, stasData3, tokenOwnerPrivateKey, stasData4, splitDestinationsForMergeSplitTx, paymentPrivateKey, utxoTemplateForFees);
-  console.log('MergeSplit Transaction Hex: ', mergeSplitHex);
+  console.log('MergeSplit Transaction Hex: ', mergeSplitHex.toString());
   const tokenUtxosFromMergeSplitTx = [utility.getUtxoFromTx(mergeSplitHex, 0), utility.getUtxoFromTx(mergeSplitHex, 1)];
 
 
   // We will transfer the first token UTXO from the mergeSplit transaction to the same owner address
   const transferHex2 = await stasTransfer.signed(tokenOwnerPrivateKey, tokenUtxosFromMergeSplitTx[0], tokenOwnerAddress, utxoTemplateForFees, paymentPrivateKey);
-  console.log('Transfer2 Transaction Hex: ', transferHex2);
+  console.log('Transfer2 Transaction Hex: ', transferHex2.toString());
   const tokenUtxoFromTransfer2 = utility.getUtxoFromTx(transferHex2, 0);
 
 
   // We will transfer the second token UTXO from the mergeSplit transaction to the same owner address
   const transferHex3 = await stasTransfer.signed(tokenOwnerPrivateKey, tokenUtxosFromMergeSplitTx[1], tokenOwnerAddress, utxoTemplateForFees, paymentPrivateKey);
-  console.log('Transfer3 Transaction Hex: ', transferHex3);
+  console.log('Transfer3 Transaction Hex: ', transferHex3.toString());
   const tokenUtxoFromTransfer3 = utility.getUtxoFromTx(transferHex3, 0);
 
 
@@ -104,25 +104,25 @@ const fullCycleTestNonBroadcast = async () => {
   // This example will take the STAS UTXO from the transferHex2 transaction and create an offer to swap it for the transactionHex3 transaction STAS UTXO
   const wantedDataForCreateSwap = {satoshis: tokenUtxoFromTransfer3.satoshis, script: tokenUtxoFromTransfer3.script};
   const offerHex = await stasCreateSwap.signed(tokenOwnerPrivateKey, tokenUtxoFromTransfer2, wantedDataForCreateSwap);
-  console.log('Offer Transaction Hex: ', offerHex);
+  console.log('Offer Transaction Hex: ', offerHex.toString());
 
 
   // We will now complete the swap by adding in all remaining parameters required to call the stasAcceptSwap function
   const swapHex = await stasAcceptSwap.signed(offerHex, tokenOwnerPrivateKey, transferHex2, transferHex3, tokenUtxoFromTransfer3.vout, paymentPrivateKey, utxoTemplateForFees);
-  console.log('Swap Transaction Hex: ', swapHex);
+  console.log('Swap Transaction Hex: ', swapHex.toString());
 
 
   // Here we will now merge the two STAS UTXOs from the atomic swap into a single 10 satoshi STAS UTXO
   const stasData5 = {txHex: swapHex, vout: 0};
   const stasData6 = {txHex: swapHex, vout: 1};
   const mergeHex2 = await stasMerge.signed(tokenOwnerPrivateKey, stasData5, tokenOwnerPrivateKey, stasData6, tokenOwnerAddress, paymentPrivateKey, utxoTemplateForFees);
-  console.log('Merge2 Transaction Hex: ', mergeHex2);
+  console.log('Merge2 Transaction Hex: ', mergeHex2.toString());
   const tokenUtxoFromMergeTx2 = utility.getUtxoFromTx(mergeHex2, 0);
 
 
   // Here we are now transferring the token to the issuer address. This is required for the STAS-20 token as only the issuer is allowed to redeem the token.
   const transferHex4 = await stasTransfer.signed(tokenOwnerPrivateKey, tokenUtxoFromMergeTx2, issuerAddress, utxoTemplateForFees, paymentPrivateKey);
-  console.log('Transfer4 Transaction Hex: ', transferHex4);
+  console.log('Transfer4 Transaction Hex: ', transferHex4.toString());
   const tokenUtxoFromTransfer4 = utility.getUtxoFromTx(transferHex4, 0);
 
 
@@ -130,13 +130,13 @@ const fullCycleTestNonBroadcast = async () => {
   // Note that the first output will always be the p2pkh redemption output which will be the conversion back to native satoshis
   const splitDestinationsForRedeemSplit = [{address: issuerAddress, satoshis: 5}];
   const redeemSplitHex = await stasRedeemSplit.signed(issuerPrivateKey, tokenUtxoFromTransfer4, splitDestinationsForRedeemSplit, utxoTemplateForFees, paymentPrivateKey);
-  console.log('RedeemSplit Transaction Hex: ', redeemSplitHex);
+  console.log('RedeemSplit Transaction Hex: ', redeemSplitHex.toString());
   const tokenUtxosFromRedeemSplitTx = utility.getUtxoFromTx(redeemSplitHex, 1);
 
 
   // Now we will redeem the final STAS UTXO back to native satoshis which completes the full cycle.
   const redeemHex = await stasRedeem.signed(issuerPrivateKey, tokenUtxosFromRedeemSplitTx, utxoTemplateForFees, paymentPrivateKey);
-  console.log('Redeem Transaction Hex: ', redeemHex);
+  console.log('Redeem Transaction Hex: ', redeemHex.toString());
 
   console.log('Full Cycle Non Broadcast Test Completed');
 };
